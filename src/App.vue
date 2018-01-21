53d48ecb6407995620e34940fd2776b94adb9386
taoxj <taoxj1992@163.com>
2018-01-22 04:18:50 +0800

    input Date

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <MobileInputDate type="daterange6" v-model="d" />
+    <button @click="d.visible=!d.visible">点击</button>
+  </div>
+</template>
+
+<script>
+import MobileInputDate from './components/MobileInputDate';
+
+export default {
+  name: 'App',
+  components: {
+    MobileInputDate
+  },
+  data() {
+    return {
+      d: {
+        visible: false,
+        startDate: '2013-06-23',
+        endDate: '2016-06-23',
+        oneDate: '2018-1-9'
+      }
+    };
+  },
+  created() {
+    window.t = this;
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
